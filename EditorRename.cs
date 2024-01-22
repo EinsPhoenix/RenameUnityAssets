ea950ace6263d354b417e4e3dbcb062b596e1289
EinsPhoenix <138807190+EinsPhoenix@users.noreply.github.com>
2024-01-22 08:48:01 +0100

    Update EditorRename.cs

--- EditorRename.cs
+++ EditorRename.cs
@@ -23,82 +23,98 @@ public class RenameAssetsScript : EditorWindow
     private Vector2 scrollPosition;
     private Regex regex;
 
-    [MenuItem("Tools/Asset Renamer")] // Hier wird der Menüpfad festgelegt
+    [MenuItem("Tools/Asset Renamer")]
     public static void ShowWindow()
     {
         EditorWindow.GetWindow<RenameAssetsScript>("Asset Renamer");
     }
+
     void OnGUI()
     {
         GUILayout.Label("Asset Renamer", EditorStyles.boldLabel);
+        GUILayout.Label("General", EditorStyles.boldLabel);
 
-        GUIContent searchTermContent = new GUIContent("Search Term:", "Enter the term to search for and you want to replace with replacement term");
+        GUIContent searchTermContent = new GUIContent("Search Term:", "Enter the term to search for and replace with the replacement term");
         searchTerm = EditorGUILayout.TextField(searchTermContent, searchTerm);
 
-        GUIContent replacementTermContent = new GUIContent("Replacement Term:", "Enter the term witch should replace the search term");
+        GUIContent replacementTermContent = new GUIContent("Replacement Term:", "Enter the term which should replace the search term");
         replacementTerm = EditorGUILayout.TextField(replacementTermContent, replacementTerm);
 
-        GUIContent renameContentsContent = new GUIContent("Would you like to change contents of Scripts with have the search term in their name?", "Enable this option if you want to change the contents of script files in addition to renaming assets. Maybe it breaks something, who knows ¯\\_(ツ)_/¯. Lets find out :D");
+        GUILayout.Space(10);
+        GUILayout.Label("Include Scripts", EditorStyles.boldLabel);
+        GUILayout.Space(5);
+
+        GUIContent renameContentsContent = new GUIContent("Include Script Content from Scripts that you've checked", "Enable this option if you want to change the contents of script files in addition to renaming assets. Maybe it breaks something, who knows ¯\\_(ツ)_/¯. Let's find out :D");
         RenameContents = EditorGUILayout.ToggleLeft(renameContentsContent, RenameContents);
 
-        GUIContent ShouldSearchallScenes = new GUIContent("Would you like to change all contents of your Scripts? (Trust me it takes a while)", "Enable this option if you want to change the contents of ALL script files in addition to renaming assets. Maybe it breaks something, who knows ¯\\_(ツ)_/¯. Lets find out :D");
-        SearchAllContents = EditorGUILayout.ToggleLeft(ShouldSearchallScenes, SearchAllContents);
+        GUIContent shouldSearchAllScenes = new GUIContent("Include content of all your Scripts? (Trust me it takes a while)", "Enable this option if you want to change the contents of ALL script files in addition to renaming assets. Maybe it breaks something, who knows ¯\\_(ツ)_/¯. Let's find out :D");
+        SearchAllContents = EditorGUILayout.ToggleLeft(shouldSearchAllScenes, SearchAllContents);
 
-        GUIContent ScenesInBuildSettings = new GUIContent("Would you like to change the Name of your GameObjects in Scene?", "Enable this option if you want to change the name from GameObjects an Prefabs that are currently in your Scenes. The Scenes must be referenced in your buildsettings! Maybe it breaks something, who knows ¯\\_(ツ)_/¯. Lets find out :D");
-        ChangeObjectsInScenes = EditorGUILayout.ToggleLeft(ScenesInBuildSettings, SearchAllContents);
+        GUILayout.Space(10);
+        GUILayout.Label("Include Scenes", EditorStyles.boldLabel);
+        GUILayout.Space(5);
+
+        GUIContent scenesInBuildSettings = new GUIContent("Would you like to change the Name of your GameObjects in your Scenes?", "Enable this option if you want to change the name of GameObjects and Prefabs that are currently in your Scenes. The Scenes must be referenced in your build settings! Maybe it breaks something, who knows ¯\\_(ツ)_/¯. Let's find out :D");
+        ChangeObjectsInScenes = EditorGUILayout.ToggleLeft(scenesInBuildSettings, ChangeObjectsInScenes);
 
         GUILayout.Space(20);
+        GUILayout.Label("Check the Assets you want to rename", EditorStyles.boldLabel);
+        GUILayout.Space(5);
 
-        float scrollViewHeight = Mathf.Min(800, position.height - 200);
-        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUILayout.Height(scrollViewHeight));
+        float scrollViewHeight = Mathf.Min(500, position.height - 200);
+scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUILayout.Height(scrollViewHeight));
 
-        assetGuids = AssetDatabase.FindAssets("t:Object", null);
-        foreach (string assetGuid in assetGuids)
+assetGuids = AssetDatabase.FindAssets("t:Object", null);
+foreach (string assetGuid in assetGuids)
+{
+    string assetPath = AssetDatabase.GUIDToAssetPath(assetGuid);
+    string assetName = Path.GetFileNameWithoutExtension(assetPath);
+
+    if (assetName.IndexOf(searchTerm, StringComparison.OrdinalIgnoreCase) != -1)
+    {
+        bool isSelected = selectedAssets.Contains(assetPath);
+        bool newSelection = EditorGUILayout.ToggleLeft(assetName, isSelected);
+
+        if (newSelection != isSelected)
         {
-            string assetPath = AssetDatabase.GUIDToAssetPath(assetGuid);
-            string assetName = Path.GetFileNameWithoutExtension(assetPath);
-
-            if (assetName.IndexOf(searchTerm, StringComparison.OrdinalIgnoreCase) != -1)
-            {
-                bool isSelected = selectedAssets.Contains(assetPath);
-                bool newSelection = EditorGUILayout.ToggleLeft(assetName, isSelected);
-
-                if (newSelection != isSelected)
-                {
-                    if (newSelection)
-                        selectedAssets.Add(assetPath);
-                    else
-                        selectedAssets.Remove(assetPath);
-                }
-            }
-        }
-
-        EditorGUILayout.EndScrollView();
-
-        GUILayout.FlexibleSpace();
-
-        if (GUILayout.Button("Rename Assets", GUILayout.Height(30)))
-        {
-            if (EditorUtility.DisplayDialog("Confirm", "Do you really want to risk it and rename your assets? It could break your project, do you have an bakeup? (Just in case)", "Yes", "No"))
-            {
-                if (!string.IsNullOrEmpty(searchTerm))
-                {
-                    if (ChangeObjectsInScenes)
-                    {
-                        SearchAndReplaceInAllScenes();
-                    }
-
-                    RenameAssets();
-                    EditorUtility.DisplayDialog("It's ready", "The renaming was succesfully! I knew it!", "Ok");
-                }
-                else
-                {
-                    EditorUtility.DisplayDialog("Whoops, there was a mistake", "You've forgotten the Search Term, please enter one", "Ok");
-                }
-            }
+            if (newSelection)
+                selectedAssets.Add(assetPath);
+            else
+                selectedAssets.Remove(assetPath);
         }
     }
+}
 
+EditorGUILayout.EndScrollView();
+
+GUILayout.BeginHorizontal();
+
+GUILayout.FlexibleSpace();
+
+if (GUILayout.Button("Rename Assets", GUILayout.Height(30), GUILayout.ExpandWidth(true)))
+{
+    if (EditorUtility.DisplayDialog("Confirm", "Do you really want to risk it and rename your assets? It could break your project. Do you have a backup? (Just in case)", "Yes", "No"))
+    {
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            if (ChangeObjectsInScenes)
+            {
+                SearchAndReplaceInAllScenes();
+            }
+
+            RenameAssets();
+            EditorUtility.DisplayDialog("It's ready", "The renaming was successful! I knew it!", "Ok");
+        }
+        else
+        {
+            EditorUtility.DisplayDialog("Whoops, there was a mistake", "You've forgotten the Search Term, please enter one", "Ok");
+        }
+    }
+}
+
+GUILayout.Space(20);
+
+GUILayout.EndHorizontal();}
 
     void RenameAssets()
     {
@@ -111,9 +127,8 @@ public class RenameAssetsScript : EditorWindow
                 try
                 {
                     string assetPath = AssetDatabase.GUIDToAssetPath(assetGuid);
-                    string assetName = Path.GetFileNameWithoutExtension(assetPath);
+                                        string assetName = Path.GetFileNameWithoutExtension(assetPath);
                     string extension = Path.GetExtension(assetPath);
-
 
                     if (extension.Equals(".cs", StringComparison.OrdinalIgnoreCase))
                     {
@@ -125,15 +140,12 @@ public class RenameAssetsScript : EditorWindow
                         File.WriteAllLines(assetPath, scriptLines);
 
                         Debug.Log("The content of Script has been changed: " + assetPath);
-
                     }
                 }
                 catch (Exception e)
                 {
-                    Debug.LogError("Whoops, there was a mistake made: " + e.Message);
+                    Debug.LogError("Whoops, there was a mistake: " + e.Message);
                 }
-
-
             }
 
             foreach (string assetPath in selectedAssets)
@@ -171,7 +183,7 @@ public class RenameAssetsScript : EditorWindow
                 }
                 catch (Exception e)
                 {
-                    Debug.LogError("Whoops, there was a mistake made: " + e.Message);
+                    Debug.LogError("Whoops, there was a mistake: " + e.Message);
                 }
             }
 
@@ -186,7 +198,6 @@ public class RenameAssetsScript : EditorWindow
             EditorSceneManager.OpenScene(scene.path);
             SearchAndReplace();
             SaveScene(scene.path);
-
         }
     }
 
@@ -201,26 +212,15 @@ public class RenameAssetsScript : EditorWindow
             {
                 Undo.RecordObject(obj, "Name replace");
                 obj.name = obj.name.Replace(searchTerm, replacementTerm);
-                Debug.Log("New name" + obj.name);
+                Debug.Log("New name: " + obj.name);
                 EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
-
             }
         }
     }
 
-
     void SaveScene(string scenePath)
     {
         EditorSceneManager.SaveScene(SceneManager.GetActiveScene(), scenePath);
-        Debug.Log("Szene was saved: " + scenePath);
+        Debug.Log("Scene was saved: " + scenePath);
     }
-
 }
-
-
-
-
-
-//18.1.2024
-//Von Noah Kirsch
-
